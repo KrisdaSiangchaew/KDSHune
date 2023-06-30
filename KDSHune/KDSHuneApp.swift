58562e275bf0e1da95ac5619f5a2efb5022e2a15
KrisdaSiangchaew <67571681+KrisdaSiangchaew@users.noreply.github.com>
2023-06-30 21:26:30 +0700

    Initial Commit

---
+++ KDSHune/KDSHuneApp.swift
@@ -0,0 +1,17 @@
+//
+//  KDSHuneApp.swift
+//  KDSHune
+//
+//  Created by Kris on 30/6/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct KDSHuneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
